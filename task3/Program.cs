13cc643c2775ec9e92a79901bfe12de306b8e1da
diplg <dimas4354@icloud.com>
2022-04-27 20:22:37 +0300

    задание3

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿//количество цифр в числе
+
+//string a = Convert.ToString(199234);
+//Console.WriteLine(a.Length);
+
+int a = 1234;
+int count = 0;
+while (a > 0)
+{
+    a = a / 10;
+    count++;
+}
+Console.WriteLine(count);
+
+
+
+
+
