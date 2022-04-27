f08c83696cc4e8e1d6d1222fe7b0113fe3c37ac5
diplg <dimas4354@icloud.com>
2022-04-27 19:59:30 +0300

    задание2

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// произведение чисел от 1 до N
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int sum = 1;
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine(i);
+    sum = sum * i;
+
+}
+Console.WriteLine(sum);
