751205e910b0ff59e53c0bfda5955e31ee6ac1ff
diplg <dimas4354@icloud.com>
2022-04-27 19:36:35 +0300

    задание1

---
+++ task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 1: сумма чисел от 1 до A.
+
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine());
+int sum = 0;
+
+for(int i = 1; i <= A; i++)
+{
+    sum += i;
+    Console.WriteLine(sum);
+}
