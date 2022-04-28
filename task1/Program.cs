fe61cb44db473bccff53ea5780f38636a0e187d9
diplg <dimas4354@icloud.com>
2022-04-28 23:05:54 +0300

    изменение

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Задача 1: сумма чисел от 1 до A.
 
 Console.Write("Введите число: ");
-int A = int.Parse(Console.ReadLine());
+int A = Convert.ToInt32(Console.ReadLine());
 int sum = 0;
 
 for(int i = 1; i <= A; i++)
